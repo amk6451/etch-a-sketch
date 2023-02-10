a26b35d9d0447da7e6ee934617ed5c28f4df9a48
Alan Kwong <akwong910@gmail.com>
2023-02-09 21:56:18 -0500

    Update grid element listeners and reset button

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # etch-a-sketch
 This is an etch-a-sketch game created with HTML, CSS and JS.
 
-References:
+Supplemental References:
   /* https://developer.mozilla.org/en-US/docs/Web/CSS/Using_CSS_custom_properties */
   /* https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Grid_Layout/Basic_Concepts_of_Grid_Layout */
+  //https://www.reddit.com/r/webdev/comments/lsstjn/getelementsbyclassname_not_working/
--- etch.js
+++ etch.js
@@ -5,9 +5,49 @@ function makeRows(rows, cols) {
   container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
-    // cell.innerText = (c + 1);
-    container.appendChild(cell).className = "grid-item";
+    cell.classList.add('gridElement');
+
+    cell.innerText = (c + 1);
+    container.appendChild(cell);
   };
 };
 
-makeRows(10, 10);
+function clearGrid() {
+
+    let containerReset = document.getElementById('container');
+    while (containerReset.firstChild) {
+        containerReset.removeChild(containerReset.firstChild);
+      }
+     return main()
+    }
+
+
+
+
+function main(){
+
+makeRows(9,9);
+
+
+const elementReset = document.getElementById("reset");
+elementReset.addEventListener("click", clearGrid);
+
+const squares = document.querySelectorAll('.gridElement');
+
+
+squares.forEach( (option) => {
+    option.addEventListener("mousemove", () => {
+        option.setAttribute("class", "greyColor");
+    }
+
+    );
+});
+
+};
+
+main();
+
+
+
+ 
+
